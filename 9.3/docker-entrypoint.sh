ae759ff77b181c49a8ff301ae197535225ea3add
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-01-27 15:39:37 -0600

    Changed few default behaviour Used postgres as password by default Added option to create an extra superuser

--- 9.3/docker-entrypoint.sh
+++ 9.3/docker-entrypoint.sh
@@ -1,55 +1,68 @@
 #!/bin/bash
 set -e
 
+startpgback() {
+    gosu postgres pg_ctl start
+}
+
+stoppg() {
+    gosu postgres pg_ctl stop
+}
+
+preparepg() {
+    # setup database
+	gosu postgres initdb
+    startpgback
+
+    # account setup
+    if [ ${POSTGRES_PASSWORD+defined} ]
+    then
+        pass='$POSTGRES_PASSWORD'
+    else
+        cat >&2 <<-'EOWARN'
+            ****************************************************
+                No password has been given for superuser postgres.
+                Using default password *postgres*
+            ****************************************************
+        EOWARN
+        pass='postgres'
+    fi
+    gosu postgres psql -U postgres -c "ALTER ROLE postgres WITH ENCRYPTED PASSWORD $pass"
+    
+    # create another superuser 
+    if [ ${SUPERUSER+defined} -a ${SUPERPASS+defined} ]
+    then
+        gosu postgres createuser  -U postgres -d -E -l -s $SUPERUSER
+        gosu postgres psql -U postgres -c "ALTER ROLE docker PASSWORD '$SUPERPASS'"
+        PGPASSWORD=$SUPERPASS createdb -U $SUPERUSER $SUPERUSER
+    fi
+
+    # create folders
+    if [ ! -e $BACKUP ]; then 
+        mkdir -p $BACKUP
+        chown postgres:postgres $BACKUP
+    fi
+
+    if [ ! -e $ARCHIVE ]; then 
+        mkdir -p $ARCHIVE
+        chown postgres:postgres $ARCHIVE
+    fi
+    stoppg
+}
+
 if [ "$1" = 'postgres' ]; then
-	chown -R postgres "$PGDATA"
+	chown -R postgres $PGDATA
 	
 	if [ -z "$(ls -A "$PGDATA")" ]; then
 		gosu postgres initdb
-		
-		sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
-		
-		# check password first so we can ouptut the warning before postgres
-		# messes it up
-		if [ "$POSTGRES_PASSWORD" ]; then
-			pass="PASSWORD '$POSTGRES_PASSWORD'"
-			authMethod=md5
-		else
-			# The - option suppresses leading tabs but *not* spaces. :)
-			cat >&2 <<-'EOWARN'
-				****************************************************
-				WARNING: No password has been set for the database.
-				         Use "-e POSTGRES_PASSWORD=password" to set
-				         it in "docker run".
-				****************************************************
-			EOWARN
-			
-			pass=
-			authMethod=trust
-		fi
-		
-		: ${POSTGRES_USER:=postgres}
-		if [ "$POSTGRES_USER" = 'postgres' ]; then
-			op='ALTER'
-		else
-			op='CREATE'
-			gosu postgres postgres --single -jE <<-EOSQL
-				CREATE DATABASE "$POSTGRES_USER" ;
-			EOSQL
-			echo
-		fi
-		
-		gosu postgres postgres --single -jE <<-EOSQL
-			$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
-		EOSQL
-		echo
-		
-		{ echo; echo "host all \"$POSTGRES_USER\" 0.0.0.0/0 $authMethod"; } >> "$PGDATA"/pg_hba.conf
+        preparepg
 		
 		if [ -d /docker-entrypoint-initdb.d ]; then
+            startpgback
 			for f in /docker-entrypoint-initdb.d/*.sh; do
 				[ -f "$f" ] && . "$f"
 			done
+            stoppg
 		fi
 	fi
 	
