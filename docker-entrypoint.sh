54a3f2adf77cc8c73e8f6dafce699862b29e4bd5
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-01-28 16:28:21 -0600

    Added addition of custom config files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,56 +1,66 @@
 #!/bin/bash
-set -e
+set -x
+
+startpgback() {
+    logger -s starting postgresql service
+    gosu postgres pg_ctl start
+    sleep 3
+}
+
+stoppg() {
+    logger -s stopping postgresql service
+    gosu postgres pg_ctl stop
+}
+
+preparepg() {
+    # setup database
+	gosu postgres initdb
+    
+    #Add the custom config file
+    mv /pg_hba.conf /var/lib/postgresql/data/
+
+    startpgback
+
+    # account setup
+    if [ ${POSTGRES_PASSWORD+defined} ]
+    then
+        pass="'$POSTGRES_PASSWORD'"
+    else
+        logger -s going to use default pass
+        pass="'postgres'"
+    fi
+    logger -s "going to use password $pass"
+    gosu postgres psql -U postgres -c "ALTER ROLE postgres WITH ENCRYPTED PASSWORD $pass"
+    
+    # create another superuser 
+    if [ ${SUPERUSER+defined} -a ${SUPERPASS+defined} ]
+    then
+        gosu postgres createuser  -U postgres -d -E -l -s $SUPERUSER
+        gosu postgres psql -U postgres -c "ALTER ROLE $SUPERUSER PASSWORD '$SUPERPASS'"
+        PGPASSWORD=$SUPERPASS createdb -U $SUPERUSER $SUPERUSER
+    fi
+    stoppg
+}
 
 if [ "$1" = 'postgres' ]; then
-	chown -R postgres "$PGDATA"
+	chown -R postgres $PGDATA
 	
 	if [ -z "$(ls -A "$PGDATA")" ]; then
-		gosu postgres initdb
-		
-		sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
-		
-		# check password first so we can ouptut the warning before postgres
-		# messes it up
-		if [ "$POSTGRES_PASSWORD" ]; then
-			pass="PASSWORD '$POSTGRES_PASSWORD'"
-			authMethod=md5
-		else
-			# The - option suppresses leading tabs but *not* spaces. :)
-			cat >&2 <<-'EOWARN'
-				****************************************************
-				WARNING: No password has been set for the database.
-				         Use "-e POSTGRES_PASSWORD=password" to set
-				         it in "docker run".
-				****************************************************
-			EOWARN
-			
-			pass=
-			authMethod=trust
-		fi
-		
-		: ${POSTGRES_USER:=postgres}
-		if [ "$POSTGRES_USER" = 'postgres' ]; then
-			op='ALTER'
-		else
-			op='CREATE'
-			gosu postgres postgres --single -jE <<-EOSQL
-				CREATE DATABASE "$POSTGRES_USER" ;
-			EOSQL
-			echo
-		fi
-		
-		gosu postgres postgres --single -jE <<-EOSQL
-			$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
-		EOSQL
-		echo
-		
-		{ echo; echo "host all \"$POSTGRES_USER\" 0.0.0.0/0 $authMethod"; } >> "$PGDATA"/pg_hba.conf
+        preparepg
 		
 		if [ -d /docker-entrypoint-initdb.d ]; then
+            startpgback
 			for f in /docker-entrypoint-initdb.d/*.sh; do
 				[ -f "$f" ] && . "$f"
 			done
+            stoppg
 		fi
+
+        #copy custom config
+        if [ -d /config ]
+        then
+            mv /config/*.conf ${PGDATA}/
+        fi
 	fi
 	
 	exec gosu postgres "$@"
