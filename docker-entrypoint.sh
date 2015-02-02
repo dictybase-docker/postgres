8ffa19cffdea95472c5e91c1f7a47d7775ad947a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-02-02 13:35:05 -0600

    replaced the previous commands with single user mode statements

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-startpgback() {
-    logger -s starting postgresql service
-    gosu postgres pg_ctl start
-    sleep 3
-}
-
-stoppg() {
-    logger -s stopping postgresql service
-    gosu postgres pg_ctl stop
-}
-
 preparepg() {
     # setup database
 	gosu postgres initdb
-    
-    startpgback
 
     # account setup
     if [ ${POSTGRES_PASSWORD+defined} ]
@@ -24,31 +11,39 @@ preparepg() {
     else
         pass="'postgres'"
     fi
-    logger -s going to use password $pass
-    gosu postgres psql -U postgres -c "ALTER ROLE postgres WITH ENCRYPTED PASSWORD $pass"
-    
-    # create another superuser 
-    if [ "${SUPERUSER+defined}" -a "${SUPERPASS+defined}" ]
-    then
-        gosu postgres createuser  -U postgres -d -E -l -s $SUPERUSER
-        gosu postgres psql -U postgres -c "ALTER ROLE $SUPERUSER PASSWORD '$SUPERPASS'"
-        PGPASSWORD=$SUPERPASS createdb -U $SUPERUSER $SUPERUSER
-    fi
 
+    logger -s going to use password $pass
+    gosu postgres postgres --single -E <<-EOSQL
+        ALTER ROLE postgres WITH ENCRYPTED PASSWORD '$pass';
+EOSQL
+
+    # create another superuser 
+    if [ "${ADMIN_USER+defined}" -a "${ADMIN_PASS+defined}" ]
+    then
+        if [ "${ADMIN_DB+defined}" ]
+        then
+            admindb=$ADMIN_DB
+        else
+            admindb=$ADMIN_USER
+        fi
+        logger -s going to create admin database
+        gosu postgres postgres --single -E <<-EOSQL
+            CREATE ROLE $ADMIN_USER WITH SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD '$ADMIN_PASS';
+            CREATE DATABASE $admindb WITH OWNER $ADMIN_USER;
+EOSQL
+    fi
+        
     #Add the custom config file
     #After this every command needs to supply a password
     mv /pg_hba.conf $PGDATA/
     mv /postgresql.conf $PGDATA/
-    stoppg
 }
 
 if [ "$1" = 'postgres' ]; then
 	chown -R postgres $PGDATA
 
-	
 	if [ -z "$(ls -A "$PGDATA")" ]; then
-        preparepg
-		
+
         if ! [ -d ${PGDATA}/conf.d ]
         then
             mkdir -p ${PGDATA}/conf.d
@@ -56,11 +51,9 @@ if [ "$1" = 'postgres' ]; then
         fi
 
 		if [ -d /docker-entrypoint-initdb.d ]; then
-            startpgback
 			for f in /docker-entrypoint-initdb.d/*.sh; do
 				[ -f "$f" ] && . "$f"
 			done
-            stoppg
 		fi
 
         #copy custom config
