8741976bb1f0edbc46aadcd9c8f7d13b6f18e711
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-09-30 22:28:58 -0500

    Use psql to run all initial setup commands Added an ability to run sql file during initialization

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,10 +20,18 @@ preparepg() {
     fi
 
     logger -s going to use password $pass
-    gosu postgres postgres --single -E <<-EOSQL
+    echo "host all all 0.0.0.0/0 md5" >> ${PGDATA}/pg_hba.conf
+    # internal start of server in order to allow set-up using psql-client       
+    # does not listen on TCP/IP and waits until start finishes
+    gosu postgres pg_ctl -D "$PGDATA" \
+        -o "-c listen_addresses=''" \
+        -w start
+
+    psql --username postgres <<-EOSQL
         ALTER ROLE postgres WITH ENCRYPTED PASSWORD '$pass';
 EOSQL
 
+    # given the options create another admin user
     if [ -e "/secrets/adminuser" -a -e "/secrets/adminpass" ]
     then
         ADMIN_USER=$(cat /secrets/adminuser)
@@ -34,7 +42,6 @@ EOSQL
         fi
     fi
 
-    # create another superuser 
     if [ "${ADMIN_USER+defined}" -a "${ADMIN_PASS+defined}" ]
     then
         if [ "${ADMIN_DB+defined}" ]
@@ -44,20 +51,20 @@ EOSQL
             admindb=$ADMIN_USER
         fi
         logger -s going to create admin database
-        gosu postgres postgres --single -E <<-EOSQL
+        psql --username postgres <<-EOSQL
             CREATE ROLE $ADMIN_USER WITH SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD '$ADMIN_PASS';
             CREATE DATABASE $admindb WITH OWNER $ADMIN_USER;
 EOSQL
     fi
         
-    #Add the custom config file
-    #After this every command needs to supply a password
-    mv /pg_hba.conf $PGDATA/
-    mv /postgresql.conf $PGDATA/
 }
 
 if [ "$1" = 'postgres' ]; then
+    mkdir -p $PGDATA
 	chown -R postgres $PGDATA
+
+    chmod g+s /run/postgresql
+    chown -R postgres /run/postgresql
 
 	if [ ! -e "$PGDATA/PG_VERSION" ]; then
         preparepg
@@ -67,18 +74,32 @@ if [ "$1" = 'postgres' ]; then
             mkdir -p ${PGDATA}/conf.d
             chown -R postgres ${PGDATA}/conf.d
         fi
-
+ 
 		if [ -d /docker-entrypoint-initdb.d ]; then
-			for f in /docker-entrypoint-initdb.d/*.sh; do
-				[ -f "$f" ] && . "$f"
+			for f in /docker-entrypoint-initdb.d/*; do
+                case "$f" in
+                    *.sh)  echo "$0: running $f"; . "$f" ;;
+                    *.sql) echo "$0: running $f"; psql --username postgres < "$f" && echo ;; 
+                    *)     echo "$0: ignoring $f" ;;
+                esac
 			done
 		fi
+
+
+        #Add the custom config file
+        #After this every command needs to supply a password
+        mv /pg_hba.conf $PGDATA/
+        mv /postgresql.conf $PGDATA/
 
         #copy custom config
         if [ -d /config ]
         then
             mv /config/*.conf ${PGDATA}/conf.d/
         fi
+
+        # stop the server
+        gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+        echo 'PostgreSQL init process complete; ready for start up.'
 	fi
 	
 	exec gosu postgres "$@"
